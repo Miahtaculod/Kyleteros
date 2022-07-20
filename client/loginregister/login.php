4ee1c3b377364ae90312097988fb5241eaf5fef9
Miahtaculod <100905531+Miahtaculod@users.noreply.github.com>
2022-07-20 15:59:47 +0800

    loginregister

---
+++ client/loginregister/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css">
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+        <title>Login</title>
+    </header>
+    <body>
+        <div class="containter">
+        <form class="login-email">
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+            <div class="input-group">
+                <input type="email" placeholder="Email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="input-group">
+                <button class="btn">Submit</button>
+            </div>
+            <p class="login-register-text">Don't have an account? <a href="register.php">Register here</a></p>
+        </form>
+        </div>
+    </body>
+</html>
---
+++ client/loginregister/register.php
@@ -0,0 +1,75 @@
+<?php
+
+include'../admin/database/dbController.php';
+
+error_reporting(0);
+
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+    $cpassword = md5($_POST['cpassword']);
+
+    if ($password == $cpassword) {
+        $sql = "SELECT * FROM user WHERE email='$email'";
+        $result = mysqli_query($db_admin, $sql);
+        if(!$result->num_rows > 0){
+            $sql = "INSERT INTO user(username, email, password, cpassword)
+                    VALUES ('$username', '$email', '$password', '$cpassword')";
+            $result = mysqli_query($db_admin, $sql);
+            if($result){
+                echo "<script>alert('Registered Complete')</script>";
+                $username ="";
+                $email ="";
+                $_POST['password'] = "";
+                $_POST['cpassword'] = "";
+            } else {
+                echo "<script>alert('Something went wrong')</script>";   
+            } 
+        } else {
+            echo "<script>alert('Email already exist')</script>";   
+        }
+    } else{
+        echo "<script>alert('Password not matched')</script>";
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <header>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css">
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+        <title>Register Form</title>
+    </header>
+    <body>
+        <div class="containter">
+        <form action="" method="POST" class="login-email">
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Register</p>
+            <div class="input-group">
+                <input type="text" placeholder="Username" name="username" value="<?php echo $username ?>" required>
+            </div>
+            <div class="input-group">
+                <input type="email" placeholder="Email" name="email" value="<?php echo $email ?>" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password'] ?>" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo $_POST['cpassword'] ?>" required>
+            </div>
+            <div class="input-group">
+                <button name="submit" class="btn">Register</button>
+            </div>
+            <p class="login-register-text">Have an account? <a href="login.php">Login here</a></p>
+        </form>
+        </div>
+    </body>
+</html>
